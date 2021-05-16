31438c85a4795727ca694ba4e80febcfab68bfec
unknown <fk331998@gmail.com>
2021-05-16 18:36:19 +0530

    responsive

---
+++ src/screens/NavHeader.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import DrawerCompo from "../components/Drawer/DrawerCompo";
+import ToolbarCompo from "../components/Toolbar/ToolbarCompo";
+
+const NavHeader = () => {
+  const [left, setleft] = useState(false);
+
+  const toggleDrawer = () => {
+    setleft(false);
+  };
+  const openDrawer = () => {
+    setleft(true);
+  };
+  return (
+    <div>
+      <ToolbarCompo openDrawerHandler={openDrawer} />
+      <DrawerCompo left={left} toggleDrawerHandler={toggleDrawer} />
+    </div>
+  );
+};
+
+export default NavHeader;
